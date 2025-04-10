0251cdb481edb4391d7afc77b5637bc2b98311c2
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2025-04-10 20:13:23 +0100

    made changes in dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,19 +1,101 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link
-			href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.css"
+<html>
+	<head>
+		<title>Desired Layout</title>
+		<link
 			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
 		/>
+		<style>
+			.page-header {
+				background-color: #f0f0f0;
+				height: 10vh;
+				display: flex;
+				align-items: center;
+			}
 
-		<link rel="icon" href="./img/cohtech-logo.png" type="image/x-icon" />
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+			.sidebar {
+				width: 250px;
+				height: 78vh;
+				background-color: #e0e0e0;
+				padding: 20px;
+			}
+
+			.content {
+				padding: 20px;
+			}
+
+			.sidebar-nav {
+				list-style: none;
+				padding: 0;
+			}
+
+			.sidebar-nav li {
+				margin-bottom: 10px;
+			}
+
+			.btn {
+				border-radius: 20px;
+			}
+
+			.header-content {
+				display: flex;
+				align-items: center;
+				justify-content: space-between; /* Space between logo and profile */
+			}
+
+			footer {
+				height: 10vh !important;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		</style>
+	</head>
+	<body>
+		<header class="page-header">
+			<div class="container">
+				<div class="header-content">
+					<img
+						src="./img/cohtech-logo-full.png"
+						alt=""
+						width="100px"
+						class="responsive-img"
+					/>
+					<a href="./profile.php" class="btn purple">Profile</a>
+				</div>
+			</div>
+		</header>
+
+		<div class="row">
+			<aside class="col s12 m4 l3 sidebar">
+				<ul class="sidebar-nav">
+					<div class="container">
+						<li><a href="#" class="black-text">Dashboard</a></li>
+						<li><a href="#" class="black-text">Courses</a></li>
+						<li><a href="#" class="black-text">Payment</a></li>
+						<li><a href="#" class="black-text">Result</a></li>
+					</div>
+					<br />
+					<div class="container">
+						<a href="" class="purple btn">logout</a>
+					</div>
+				</ul>
+			</aside>
+
+			<main class="col s12 m8 l9 content">
+				<div class="container"></div>
+			</main>
+		</div>
+		<footer class="center-align black">
+			<span class="white-text">
+				© 2025 COHTECH Obubra. All Rights Reserved.
+				<a href="./index.html" target="_blank" class="white-text underline"
+					>Back To Home</a
+				>
+			</span>
+		</footer>
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+	</body>
 </html>
-
