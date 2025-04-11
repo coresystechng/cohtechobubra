9e11fe929a6cde22f448324e398ee00322766fc6
Sean Gbadamosi <139792459+seangbadamosi@users.noreply.github.com>
2025-04-11 15:19:31 +0100

    fix dashboard page

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,14 @@
 			href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
 		/>
 
-        
+        <!-- link for font awesome icons -->
+		<link
+			rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+		/>	
+		<!-- link for material icons -->
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 		<link rel="icon" href="./img/cohtech-logo.png" type="image/x-icon" />
 		<style>
 			.page-header {
@@ -18,7 +25,7 @@
 			}
 
 			.sidebar {
-				width: 250px;
+				width: 18vw !important;
 				height: 80vh;
 				background-color: #e0e0e0;
 				padding: 20px;
@@ -38,26 +45,39 @@
 				margin-bottom: 10px;
 			}
 
-			.btn {
-				border-radius: 20px;
-			}
-
 			.header-content {
 				display: flex;
 				align-items: center;
-				justify-content: space-between;
+				/* justify-content: space-between; */
+				column-gap: 100vh;
 			}
 
 			.underline {
 				text-decoration: underline;
 			}
 
+			.theme-color-text {
+				color: #702963 !important;
+			}
+
+			.theme-color-bg {
+				background-color: #702963 !important;
+			}
 
 			footer {
 				height: 10vh !important;
 				display: flex;
 				justify-content: center;
 				align-items: center;
+			}
+
+			.dropdown-trigger{
+				position: relative;
+			}
+			
+			.dropdown-content{
+				position: absolute;
+				top: -3vw;
 			}
 		</style>
 	</head>
@@ -68,10 +88,19 @@
 					<img
 						src="./img/cohtech-logo-full.png"
 						alt=""
-						width="100px"
+						width="120px"
 						class="responsive-img"
 					/>
-					<a href="./profile.php" class="btn purple">Profile</a>
+					<a href="./profile.php " class="dropdown-trigger" data-target="dropdown1">
+						<i class="fa-solid fa-user fa-xl theme-color-text"></i>
+						<i class="material-icons theme-color-text">arrow_drop_down</i>
+					</a>
+
+
+					<ul id="dropdown1" class="dropdown-content">
+  						<li><a href="./profile.php" class="theme-color-text">profile</a></li>
+  						<li><a href="./logout.php" class="red-text">logout</a></li>
+					</ul>
 				</div>
 			</div>
 		</header>
@@ -80,14 +109,40 @@
 			<aside class="col s12 m4 l3 sidebar">
 				<ul class="sidebar-nav">
 					<div class="container">
-						<li><a href="#" class="black-text">Dashboard</a></li>
-						<li><a href="#" class="black-text">Courses</a></li>
-						<li><a href="#" class="black-text">Payment</a></li>
-						<li><a href="#" class="black-text">Result</a></li>
+						<li>
+							<a href="#" class="black-text">
+							<i class="fa-solid fa-chart-column"></i>			
+							Dashboard			
+							</a>
+						</li>
+						<li>
+							<a href="#" class="black-text">
+							<i class="fa-solid fa-book"></i>	
+							Courses
+							</a>
+						</li>
+						<li>
+							<a href="#" class="black-text">
+							<i class="fa-solid fa-money-bill"></i>	
+							Payment
+							</a>
+						</li>
+						<li>
+							<a href="#" class="black-text">
+							<i class="fa-solid fa-circle-check lg"></i>	
+							Result
+							</a>
+						</li>
+						<li>
+							<a href="#" class="black-text">
+							<i class="fa-solid fa-user"></i>
+							profile
+							</a>
+						</li>
 					</div>
 					<br />
 					<div class="container">
-						<a href="./logout.php" class="purple btn">logout</a>
+						<a href="./logout.php" class="theme-color-bg btn">logout</a>
 					</div>
 				</ul>
 			</aside>
@@ -105,6 +160,12 @@
 			</span>
 		</footer>
 
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		<script>
+			$(document).ready(function() {
+  		      $('.dropdown-trigger').dropdown();
+			});
+		</script>
 	</body>
 </html>
